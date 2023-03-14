419170a4ae0a3e982e77b9b0428a295dd09c35f9
sumit anand <79632416+lostboyanand@users.noreply.github.com>
2023-03-15 05:23:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add event listener for page load
+window.addEventListener('load', function() {
+    // Add class to overlay element to make it visible
+    document.querySelector('.overlay').classList.add('visible');
+  });
+  
